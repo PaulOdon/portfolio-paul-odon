11b72c5aae35f55564f1fca6704114461bc46b2a
Paul <andrianantenainapaulodon@gmail.com>
2025-03-16 15:55:54 +0300

    v0.4

--- src/components/contacts/index.tsx
+++ src/components/contacts/index.tsx
@@ -1,20 +1,22 @@
 import React from "react";
-import { LinkIcon } from "@heroicons/react/20/solid";
-import { DevicePhoneMobileIcon } from "@heroicons/react/24/outline";
+import {
+  AtSymbolIcon,
+  DevicePhoneMobileIcon,
+} from "@heroicons/react/24/outline";
 
 export default function Contacts() {
   return (
     <div className="flex flex-col gap-4 text-white py-8">
       <div className="flex flex-col-reverse gap-4 items-center">
         <div className="flex items-center gap-1">
-          <p>Portfolio v0.3</p>
+          <p>Portfolio v0.4</p>
         </div>
         <div className="flex items-center gap-1">
           <DevicePhoneMobileIcon width={16} />
           <p>+261 34 28 622 06</p>
         </div>
         <div className="flex items-center gap-1">
-          <LinkIcon width={16} />
+          <AtSymbolIcon width={16} />
           <p>andrianantenainapaulodon@gmail.com</p>
         </div>
       </div>
